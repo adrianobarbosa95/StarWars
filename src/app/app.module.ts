b23ff3966451cdc322a418aad4d4e54c8ec3b05d
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-02-24 20:49:25 -0300

    criar projeto e importa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { StarwarsComponent } from './starwars/starwars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarwarsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
